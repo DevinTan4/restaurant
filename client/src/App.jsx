8d84623cf8f703ede76efc118464ebd5a653e652
DevinTan <vninecoc1@gmail.com>
2024-05-18 16:05:57 +0700

    template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Routes>
+        {/* <Route path="/" element={}  /> */}
+      </Routes>
+    </Fragment>
+  )
+}
+
+export default App;
