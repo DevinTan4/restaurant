07fd5a9419b1b678ed6abdcf2cf4354eccd719fb
DevinTan <vninecoc1@gmail.com>
2024-05-19 23:52:00 +0700

    feat: add eyeIcon to password input form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/register";
+import LoginPage from "./pages/login";
 
 const App = () => {
   return (
     <Fragment>
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Fragment>
   );
